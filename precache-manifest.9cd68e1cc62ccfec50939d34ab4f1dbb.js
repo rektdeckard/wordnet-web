8eec501ee7458aa7d480f7aa2953d16b14e315e8
Tobias Fried <friedtm@gmail.com>
2020-01-20 03:03:05 -0500

    Updates

---
+++ precache-manifest.9cd68e1cc62ccfec50939d34ab4f1dbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea928d2b88714141ac230758d3db0ea6",
+    "url": "/wordnet-web/index.html"
+  },
+  {
+    "revision": "9f0f49590cebd9fc5c29",
+    "url": "/wordnet-web/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "9f0f49590cebd9fc5c29",
+    "url": "/wordnet-web/static/js/2.cf203d65.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/wordnet-web/static/js/2.cf203d65.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d4595f193ba5aaf16ea7",
+    "url": "/wordnet-web/static/js/main.88417667.chunk.js"
+  },
+  {
+    "revision": "3d63f7e1318a0c24ed84",
+    "url": "/wordnet-web/static/js/runtime-main.c660572a.js"
+  }
+]);
