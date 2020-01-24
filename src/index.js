59af732cb8eace80c66e24f72baa21d60c52a3bc
rektdeckard <friedtm@gmail.com>
2020-01-24 17:44:59 -0500

    Prepare for add amplify

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { HashRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
+// import Amplify from 'aws-amplify';
+// import awsconfig from './aws-exports';
+// import { withAuthenticator } from 'aws-amplify-react';
 
 import App from "./components/App";
 import store from "./reducers";
+
+// Amplify.configure(awsconfig);
+// const AppWithAuthentication = withAuthenticator(App, true);
 
 ReactDOM.render(
   <Provider store={store}>
