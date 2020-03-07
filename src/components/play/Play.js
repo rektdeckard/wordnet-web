1c2f7ff9e68f5dd14af6fc436829e65209f67f80
Tobias Fried <friedtm@gmail.com>
2020-03-06 19:42:31 -0500

    Remove unused imports

--- src/components/play/Play.js
+++ src/components/play/Play.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import { Layout, Input } from "antd";
 import { ResponsiveNetwork } from "@nivo/network";
