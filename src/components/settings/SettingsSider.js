51793f99777c97e7b39fd0d50d0bf4e212d8fce0
Tobias Fried <friedtm@gmail.com>
2020-04-11 01:15:23 -0400

    Settings: extract datavis settings into modular side panel
    This moves all datavis controls into a reusable sider which can be added
    to any view. We plan to add this to the Session view, hidden by default,
    in case a more curious user would like to play with the settings.

---
+++ src/components/settings/SettingsSider.js
@@ -0,0 +1,300 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Layout, Menu, Select, InputNumber, Row, Col, Switch } from "antd";
+import {
+  DotChartOutlined,
+  EllipsisOutlined,
+  ForkOutlined,
+  DoubleRightOutlined,
+} from "@ant-design/icons";
+
+import {
+  setType,
+  setRepulsivity,
+  setIterations,
+  setDefaultNodeSize,
+  setNodeScale,
+  setAutoScaleSpringLength,
+  setDefaultSpringLength,
+  setMotionStiffness,
+  setMotionDamping,
+  setMotionThreshold,
+  setMaxSpeed,
+  setAnimate,
+} from "../../actions";
+import { COLORS } from "../../data/constants";
+
+const { Sider } = Layout;
+const { SubMenu } = Menu;
+const { Option } = Select;
+
+const SettingsSider = (props) => {
+  const { defaultCollapsed } = props;
+  const {
+    type,
+    repulsivity,
+    iterations,
+    defaultNodeSize,
+    nodeScale,
+    autoScaleSpringLength,
+    defaultSpringLength,
+    motionStiffness,
+    motionDamping,
+    motionThreshold,
+    maxSpeed,
+    animate,
+  } = props.settings;
+
+  return (
+    <Sider
+      style={{ background: COLORS.CARD_BACKGROUND }}
+      width={300}
+      collapsible={Boolean(defaultCollapsed)}
+      defaultCollapsed={Boolean(defaultCollapsed)}
+      collapsedWidth={0}
+      breakpoint={defaultCollapsed ? null : "lg"}
+      theme="light"
+    >
+      <Menu
+        mode="inline"
+        selectable={false}
+        defaultOpenKeys={["sim", "nodes", "edges", "motion"]}
+        style={{ height: "100%" }}
+      >
+        <SubMenu
+          key="sim"
+          title={
+            <span>
+              <DotChartOutlined />
+              Simulation
+            </span>
+          }
+        >
+          <Menu.Item key="type">
+            <Row>
+              <Col span={12}>Type</Col>
+              <Col span={4}>
+                <Select value={type} onChange={props.setType}>
+                  <Option value="force">Force</Option>
+                  <Option value="circle">Circle</Option>
+                  <Option value="concentric">Concentric</Option>
+                  <Option value="grid">Grid</Option>
+                  <Option value="radial">Radial</Option>
+                  <Option value="dagre">Dagre</Option>
+                </Select>
+              </Col>
+            </Row>
+          </Menu.Item>
+          <Menu.Item key="repulsivity">
+            <Row>
+              <Col span={12}>Repulsivity</Col>
+              <Col span={4}>
+                <InputNumber
+                  size="small"
+                  min={0}
+                  step={1000}
+                  value={repulsivity}
+                  onChange={props.setRepulsivity}
+                />
+              </Col>
+            </Row>
+          </Menu.Item>
+          <Menu.Item key="iterations">
+            <Row>
+              <Col span={12}>Iterations</Col>
+              <Col span={4}>
+                <InputNumber
+                  size="small"
+                  min={0}
+                  max={100000}
+                  step={100}
+                  value={iterations}
+                  onChange={props.setIterations}
+                />
+              </Col>
+            </Row>
+          </Menu.Item>
+        </SubMenu>
+        <SubMenu
+          key="nodes"
+          title={
+            <span>
+              <EllipsisOutlined />
+              Nodes
+            </span>
+          }
+        >
+          <Menu.Item key="size">
+            <Row>
+              <Col span={12}>Default Size</Col>
+              <Col span={4}>
+                <InputNumber
+                  size="small"
+                  min={0}
+                  value={defaultNodeSize}
+                  onChange={props.setDefaultNodeSize}
+                />
+              </Col>
+            </Row>
+          </Menu.Item>
+          <Menu.Item key="degreescale">
+            <Row>
+              <Col span={12}>Degree Scaling</Col>
+              <Col span={4}>
+                <InputNumber
+                  size="small"
+                  min={0}
+                  max={20}
+                  precision={1}
+                  step={1}
+                  value={nodeScale}
+                  onChange={props.setNodeScale}
+                />
+              </Col>
+            </Row>
+          </Menu.Item>
+        </SubMenu>
+        <SubMenu
+          key="edges"
+          title={
+            <span>
+              <ForkOutlined />
+              Edges
+            </span>
+          }
+        >
+          <Menu.Item key="autoscale">
+            <Row>
+              <Col span={12}>Auto-scaling</Col>
+              <Col span={4}>
+                <Switch
+                  size="small"
+                  checked={autoScaleSpringLength}
+                  onChange={props.setAutoScaleSpringLength}
+                />
+              </Col>
+            </Row>
+          </Menu.Item>
+          <Menu.Item key="length">
+            <Row>
+              <Col span={12}>Default Length</Col>
+              <Col span={4}>
+                <InputNumber
+                  size="small"
+                  min={0}
+                  step={10}
+                  value={defaultSpringLength}
+                  onChange={props.setDefaultSpringLength}
+                  disabled={autoScaleSpringLength}
+                />
+              </Col>
+            </Row>
+          </Menu.Item>
+        </SubMenu>
+        <SubMenu
+          key="motion"
+          title={
+            <span>
+              <DoubleRightOutlined />
+              Motion
+            </span>
+          }
+        >
+          <Menu.Item key="animate">
+            <Row>
+              <Col span={12}>Animate</Col>
+              <Col span={4}>
+                <Switch
+                  size="small"
+                  checked={animate}
+                  onChange={props.setAnimate}
+                />
+              </Col>
+            </Row>
+          </Menu.Item>
+          <Menu.Item key="stiffness">
+            <Row>
+              <Col span={12}>Motion Stiffness</Col>
+              <Col span={4}>
+                <InputNumber
+                  size="small"
+                  min={0}
+                  max={10000}
+                  step={100}
+                  value={motionStiffness}
+                  onChange={props.setMotionStiffness}
+                />
+              </Col>
+            </Row>
+          </Menu.Item>
+          <Menu.Item key="damping">
+            <Row>
+              <Col span={12}>Motion Damping</Col>
+              <Col span={4}>
+                <InputNumber
+                  size="small"
+                  min={0}
+                  max={1}
+                  step={0.01}
+                  precision={2}
+                  value={motionDamping}
+                  onChange={props.setMotionDamping}
+                />
+              </Col>
+            </Row>
+          </Menu.Item>
+          <Menu.Item key="threshold">
+            <Row>
+              <Col span={12}>Motion Threshold</Col>
+              <Col span={4}>
+                <InputNumber
+                  size="small"
+                  min={0}
+                  max={1}
+                  step={0.01}
+                  precision={2}
+                  value={motionThreshold}
+                  onChange={props.setMotionThreshold}
+                />
+              </Col>
+            </Row>
+          </Menu.Item>
+          <Menu.Item key="speed">
+            <Row>
+              <Col span={12}>Max Speed</Col>
+              <Col span={4}>
+                <InputNumber
+                  size="small"
+                  min={0}
+                  max={1000}
+                  step={10}
+                  value={maxSpeed}
+                  onChange={props.setMaxSpeed}
+                />
+              </Col>
+            </Row>
+          </Menu.Item>
+        </SubMenu>
+      </Menu>
+    </Sider>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return { settings: state.settings };
+};
+
+export default connect(mapStateToProps, {
+  setType,
+  setRepulsivity,
+  setIterations,
+  setDefaultNodeSize,
+  setNodeScale,
+  setAutoScaleSpringLength,
+  setDefaultSpringLength,
+  setMotionStiffness,
+  setMotionDamping,
+  setMotionThreshold,
+  setMaxSpeed,
+  setAnimate,
+})(SettingsSider);
