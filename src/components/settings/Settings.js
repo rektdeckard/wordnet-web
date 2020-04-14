2dcf5b90016816c039c3c9aa2825bfe1ec6aab1c
Tobias Fried <friedtm@gmail.com>
2020-04-14 18:57:24 -0400

    Settings: add confirmation message on settings save/restore

--- src/components/settings/Settings.js
+++ src/components/settings/Settings.js
@@ -1,25 +1,35 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Layout, Button } from "antd";
+import { Layout, Typography, Divider, Button, message } from "antd";
 
 import NetworkGraphInteractive from "../NetworkGraphInteractive";
 import SettingsSider from "./SettingsSider";
-import { restoreDefaults } from "../../actions"
+import Download from "../explore/Download";
+import { restoreDefaults } from "../../actions";
 import { useGraph } from "../../utils";
 import { Colors } from "../../data/constants";
+
 import data from "../../data/sample3.json";
 
 const { Content } = Layout;
+const { Title, Paragraph, Text } = Typography;
 
 const Settings = ({ settings, restoreDefaults }) => {
   const { nodes, links } = useGraph(data);
 
-  const saveSettings = () => {
+  const handleSaveSettings = () => {
     localStorage.setItem("settings", JSON.stringify(settings));
-  }
+    message.success("Settings saved");
+  };
+
+  const handleRestoreDefaults = () => {
+    restoreDefaults();
+    message.success("Settings restored");
+  };
 
   return (
     <>
+      <Title level={2}>Graph Visualization</Title>
       <Layout style={{ background: Colors.CARD_BACKGROUND }}>
         <SettingsSider />
         <Content>
@@ -29,8 +39,25 @@ const Settings = ({ settings, restoreDefaults }) => {
           />
         </Content>
       </Layout>
-      <Button type="primary" onClick={saveSettings}>Save Settings</Button>
-      <Button onClick={restoreDefaults}>Restore Defaults</Button>
+      <Divider />
+      <Title level={2}>Download Data</Title>
+      <Download
+        all
+        render={({ loading, onClick }) => (
+          <Button size="small" loading={loading} onClick={onClick}>
+            .xlsx
+          </Button>
+        )}
+      />
+      <Button size="small" disabled>
+        .csv
+      </Button>
+      <Divider />
+      <Title level={2}>Other Setting</Title>
+      <Button type="primary" onClick={handleSaveSettings}>
+        Save Settings
+      </Button>
+      <Button onClick={handleRestoreDefaults}>Restore Defaults</Button>
     </>
   );
 };
