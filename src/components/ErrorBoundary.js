d4c3561ec6e3036d31e595fdf5c7b53dd7ad612d
Tobias Fried <friedtm@gmail.com>
2020-05-13 14:51:38 -0400

    ErrorBoundary: minutiae

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -2,35 +2,34 @@ import React from "react";
 import { Analytics } from "aws-amplify";
 
 class ErrorBoundary extends React.Component {
-    state = {
-        errorMessage: ""
+  state = {
+    errorMessage: "",
+  };
+
+  static getDerivedStateFromError(error) {
+    return { errorMessage: error.toString() };
+  }
+
+  componentDidCatch(error, info) {
+    console.error(error);
+    // Generate error report
+    try {
+      Analytics.record({
+        name: "graph_error",
+        attributes: { error: error.message },
+      });
+    } catch (e) {
+      console.error(e.message);
+    }
+  }
+
+  render() {
+    if (this.state.errorMessage) {
+      return this.props.fallback ?? <p>{this.state.errorMessage}</p>;
     }
 
-    static getDerivedStateFromError(error) {
-        return { errorMessage: error.toString() }
-    }
-
-    componentDidCatch(error, info) {
-        console.warn(info.componentStack);
-
-        // Generate error report
-        try {
-          Analytics.record({
-            name: "graph_error",
-            attributes: { error: error.message }
-          });
-        } catch(e) {
-          console.error(e.message)
-        }
-    }
-
-    render() {
-        if (this.state.errorMessage) {
-            return this.props.fallback ?? <p>{this.state.errorMessage}</p>
-        }
-
-        return this.props.children
-    }
+    return this.props.children;
+  }
 }
 
 export default ErrorBoundary;
