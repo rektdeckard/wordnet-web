da335d3ec71175f36ef3303f8e1b8e185eb43d98
Tobias Fried <friedtm@gmail.com>
2020-05-25 18:15:41 -0400

    Assessment: drag-and-drop without copy within PictographGridAssessment
    We now use a callback on each PictographDragItem to properly remove it
    from its previous position, if it has been dragged from one grid sqare
    to another. We should definitely look into the performance implications
    of doing this...

--- src/components/assessment/PictographGridAssessment.js
+++ src/components/assessment/PictographGridAssessment.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Storage } from "aws-amplify";
-import { Layout, List, Row, Col } from "antd";
+import { Layout, List, Row, Col, Button, message } from "antd";
 
-import { Colors } from "../../data/constants";
+import { Color } from "../../data/constants";
 import PictographDragTarget from "./PictographDragTarget";
 import PictographDropTarget from "./PictographDropTarget";
 
@@ -15,7 +15,7 @@ const initializedGrid = new Array(GRID_ROWS)
   .fill(null)
   .map(() => new Array(GRID_COLUMNS).fill(null));
 
-const PictographGridAssessment = () => {
+const PictographGridAssessment = ({ nextStep, previousStep }) => {
   const [allPictographs, setAllPictographs] = useState([]);
   const [grid, setGrid] = useState(initializedGrid);
 
@@ -25,14 +25,16 @@ const PictographGridAssessment = () => {
         level: "public",
       });
       const pictographs = await Promise.all(
-        images.slice(1).map(async (image) => {
-          const imageURL = await Storage.get(image.key);
-          return {
-            name: image.key.split(/\W/)[2]?.replace(/_/g, " "),
-            src: imageURL,
-            id: image.eTag,
-          };
-        })
+        images
+          .filter((it) => it.key.endsWith(".svg"))
+          .map(async (image) => {
+            const imageURL = await Storage.get(image.key);
+            return {
+              name: image.key.split(/\W/)[2]?.replace(/_/g, " "),
+              src: imageURL,
+              id: image.eTag,
+            };
+          })
       );
       setAllPictographs(pictographs);
     };
@@ -41,57 +43,76 @@ const PictographGridAssessment = () => {
   }, []);
 
   const setGridItem = (row, col) => (pictograph) => {
-    const updatedGrid = [...grid];
-    updatedGrid[row][col] = pictograph;
-    setGrid(updatedGrid);
+    setGrid((grid) => {
+      grid[row][col] = pictograph;
+      return [...grid];
+    });
+  };
+
+  const handleNextClicked = () => {
+    if (grid.every((row) => row.every((col) => !col))) {
+      return message.info("You must add something to the scene");
+    }
+
+    nextStep();
   };
 
   return (
-    <Layout
-      style={{
-        height: "72vh",
-        marginBottom: 14,
-      }}
-    >
-      <Sider
-        width={400}
-        style={{ background: Colors.PANEL_BACKGROUND, overflowY: "scroll" }}
-        theme="light"
+    <>
+      <Layout
+        style={{
+          height: "72vh",
+          marginBottom: 14,
+        }}
       >
-        <List
-          grid={{ column: 2 }}
-          dataSource={allPictographs}
-          renderItem={(item) => (
-            <List.Item>
-              <PictographDragTarget item={item} />
-            </List.Item>
-          )}
-        />
-        {/* <Row gutter={[8, 8]}>
-          {allPictographs.map((pictograph, index) => (
-            <Col span={12} key={index}>
-              <Pictograph resource={pictograph} onMove={moveItem} />
-            </Col>
-          ))}
-        </Row> */}
-      </Sider>
-      <Content style={{ background: Colors.PANEL_BACKGROUND }}>
-        <Row gutter={[8, 8]}>
-          {grid
-            .map((row, rowIndex) =>
-              row.map((col, colIndex) => (
-                <Col span={6} key={`${rowIndex}:${colIndex}`}>
-                  <PictographDropTarget
-                    item={col}
-                    onDrop={setGridItem(rowIndex, colIndex)}
-                  />
-                </Col>
-              ))
-            )
-            .flat()}
-        </Row>
-      </Content>
-    </Layout>
+        <Sider
+          width={400}
+          style={{ background: Color.PANEL_BACKGROUND, overflowY: "scroll" }}
+          theme="light"
+        >
+          <List
+            grid={{ column: 2 }}
+            dataSource={allPictographs}
+            renderItem={(item) => (
+              <List.Item>
+                <PictographDragTarget item={item} />
+              </List.Item>
+            )}
+          />
+        </Sider>
+        <Content style={{ background: Color.PANEL_BACKGROUND }}>
+          <Row
+            gutter={[8, 8]}
+            style={{ marginTop: 8, marginLeft: 8, marginRight: 8 }}
+          >
+            {grid
+              .map((row, rowIndex) =>
+                row.map((col, colIndex) => (
+                  <Col span={6} key={`${rowIndex}:${colIndex}`}>
+                    <PictographDropTarget
+                      item={col}
+                      onDrop={setGridItem(rowIndex, colIndex)}
+                    />
+                  </Col>
+                ))
+              )
+              .flat()}
+          </Row>
+        </Content>
+      </Layout>
+      <Row gutter={8} justify="end">
+        <Col flex="100px">
+          <Button block onClick={previousStep}>
+            Previous
+          </Button>
+        </Col>
+        <Col flex="100px">
+          <Button block onClick={handleNextClicked}>
+            Next
+          </Button>
+        </Col>
+      </Row>
+    </>
   );
 };
 
